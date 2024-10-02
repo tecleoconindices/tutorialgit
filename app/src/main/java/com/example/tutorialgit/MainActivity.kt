7197c5461fe5b896b6d2962e8e41f585122a1caa
tecleolento <julippps@gmail.com>
2024-10-02 03:51:20 -0300

    incluyo el print

--- app/src/main/java/com/example/tutorialgit/MainActivity.kt
+++ app/src/main/java/com/example/tutorialgit/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        print("esto es un ejemplo")
         setContent {
             TutorialgitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
